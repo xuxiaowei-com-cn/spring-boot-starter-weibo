e8e20c08f46b4a0c0dfc896cb4c8cd6367969dad
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-11-30 21:40:53 +0800

    :bug: 修正微博用户接口反序列化错误，忽略微博用户接口反序列化时不存在的字段

--- src/main/java/org/springframework/security/oauth2/server/authorization/client/InMemoryWeiBoWebsiteService.java
+++ src/main/java/org/springframework/security/oauth2/server/authorization/client/InMemoryWeiBoWebsiteService.java
@@ -21,6 +21,7 @@ package org.springframework.security.oauth2.server.authorization.client;
  */
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.http.HttpEntity;
@@ -273,6 +274,8 @@ public class InMemoryWeiBoWebsiteService implements WeiBoWebsiteService {
 		WeiBoWebsiteTokenResponse weiBoWebsiteTokenResponse;
 		ObjectMapper objectMapper = new ObjectMapper();
 		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
 		try {
 			weiBoWebsiteTokenResponse = objectMapper.readValue(forObject, WeiBoWebsiteTokenResponse.class);
 		}
