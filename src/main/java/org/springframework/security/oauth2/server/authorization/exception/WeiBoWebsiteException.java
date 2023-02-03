114dba4a865b597b32c71311dd20d1e04c60daf2
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-02-03 15:48:57 +0800

    :truck: 重命名

--- src/main/java/org/springframework/security/oauth2/server/authorization/exception/WeiBoWebsiteException.java
+++ src/main/java/org/springframework/security/oauth2/server/authorization/exception/WeiBoWebsiteException.java
@@ -9,9 +9,9 @@ package org.springframework.security.oauth2.server.authorization.exception;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,25 +29,25 @@ import org.springframework.security.oauth2.core.OAuth2Error;
  * @author xuxiaowei
  * @since 0.0.1
  */
-public class WeiBoException extends OAuth2AuthenticationException {
+public class WeiBoWebsiteException extends OAuth2AuthenticationException {
 
-	public WeiBoException(String errorCode) {
+	public WeiBoWebsiteException(String errorCode) {
 		super(errorCode);
 	}
 
-	public WeiBoException(OAuth2Error error) {
+	public WeiBoWebsiteException(OAuth2Error error) {
 		super(error);
 	}
 
-	public WeiBoException(OAuth2Error error, Throwable cause) {
+	public WeiBoWebsiteException(OAuth2Error error, Throwable cause) {
 		super(error, cause);
 	}
 
-	public WeiBoException(OAuth2Error error, String message) {
+	public WeiBoWebsiteException(OAuth2Error error, String message) {
 		super(error, message);
 	}
 
-	public WeiBoException(OAuth2Error error, String message, Throwable cause) {
+	public WeiBoWebsiteException(OAuth2Error error, String message, Throwable cause) {
 		super(error, message, cause);
 	}
 
