49ee1a737c68a686cacc227dd9ba3704adce9ef4
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-02-22 08:50:17 +0800

    :arrow_up: spring-authorization-server 0.4.1、spring-javaformat 0.0.36

--- src/main/java/org/springframework/security/oauth2/server/authorization/authentication/OAuth2WeiBoWebsiteAuthenticationProvider.java
+++ src/main/java/org/springframework/security/oauth2/server/authorization/authentication/OAuth2WeiBoWebsiteAuthenticationProvider.java
@@ -9,9 +9,9 @@ package org.springframework.security.oauth2.server.authorization.authentication;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -123,7 +123,7 @@ public class OAuth2WeiBoWebsiteAuthenticationProvider implements AuthenticationP
 		Set<String> requestedScopes = StringUtils.commaDelimitedListToSet(grantAuthenticationToken.getScope());
 
 		OAuth2ClientAuthenticationToken clientPrincipal = OAuth2AuthenticationProviderUtils
-				.getAuthenticatedClientElseThrowInvalidClient(grantAuthenticationToken);
+			.getAuthenticatedClientElseThrowInvalidClient(grantAuthenticationToken);
 		RegisteredClient registeredClient = clientPrincipal.getRegisteredClient();
 
 		// 自定义微博开放平台 网站应用用户的IP与SessionId
